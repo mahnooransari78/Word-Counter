ac27563dce4b0e85d05b02f70585e61b45332821
mahnooransari78 <164240218+mahnooransari78@users.noreply.github.com>
2024-04-18 15:37:35 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+const answers :{
+    sentence : string;
+} = await inquirer.prompt(
+     [
+        {
+            name : "sentence",
+            type : "input",
+            message: "Enter your sectence to count the word :",
+        }
+
+    ]
+);
+const word = answers.sentence.trim().split(" ")
+
+console.log(word);
+
+console.log(` your sentence word count is ${word.length}`);
+
